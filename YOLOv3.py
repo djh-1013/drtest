50e6ab97a4fa2e4a7aac210fbcf7fd51fb65e665
a <1824708334@qq.com>
2025-09-15 15:26:47 +0800

    LG

---
+++ YOLOv3.py
@@ -0,0 +1,110 @@
+from functools import wraps
+from tensorflow.keras.layers import Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate,Input
+from tensorflow.keras.layers import LeakyReLU,BatchNormalization,MaxPooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from functools import reduce
+
+def compose(*funcs):
+    if funcs:
+        return reduce(lambda f, g: lambda *a, **kw: g(f(*a, **kw)), funcs)
+    else:
+        raise ValueError('Composition of empty sequence not supported.')
+
+#--------------------------------------------------#
+#   单次卷积
+#--------------------------------------------------#
+@wraps(Conv2D)
+def DarknetConv2D(*args, **kwargs):
+    darknet_conv_kwargs = {'kernel_regularizer': l2(5e-4)}
+    darknet_conv_kwargs['padding'] = 'valid' if kwargs.get('strides')==(2,2) else 'same'
+    darknet_conv_kwargs.update(kwargs)
+    return Conv2D(*args, **darknet_conv_kwargs)
+
+#---------------------------------------------------#
+#   卷积块
+#   DarknetConv2D + BatchNormalization + LeakyReLU
+#---------------------------------------------------#
+def DarknetConv2D_BN_Leaky(*args, **kwargs):
+    no_bias_kwargs = {'use_bias': False}
+    no_bias_kwargs.update(kwargs)
+    return compose(
+        DarknetConv2D(*args, **no_bias_kwargs),
+        BatchNormalization(),
+        LeakyReLU(alpha=0.1))
+
+#---------------------------------------------------#
+#   卷积块
+#   DarknetConv2D + BatchNormalization + LeakyReLU
+#---------------------------------------------------#
+def resblock_body(x, num_filters, num_blocks):
+    x = ZeroPadding2D(((1,0),(1,0)))(x)
+    x = DarknetConv2D_BN_Leaky(num_filters, (3,3), strides=(2,2))(x)
+    for i in range(num_blocks):
+        y = DarknetConv2D_BN_Leaky(num_filters//2, (1,1))(x)
+        y = DarknetConv2D_BN_Leaky(num_filters, (3,3))(y)
+        x = Add()([x,y])      # 残差跳连融合
+    return x
+
+#---------------------------------------------------#
+#   darknet53 的主体部分
+#---------------------------------------------------#
+def darknet_body(x):
+    x = DarknetConv2D_BN_Leaky(32, (3,3))(x)
+    x = resblock_body(x, 64, 1)
+    x = resblock_body(x, 128, 2)
+    x = resblock_body(x, 256, 8)
+    feat1 = x   # 保留darknet输出特征
+    x = resblock_body(x, 512, 8)
+    feat2 = x   # 保留darknet输出特征
+    x = resblock_body(x, 1024, 4)
+    feat3 = x   # 保留darknet输出特征
+    return feat1,feat2,feat3
+
+# ---------------------------------------------------#
+#   特征层->最后的输出
+# ---------------------------------------------------#
+def make_last_layers(x, num_filters, out_filters):
+    # 五次卷积
+    x = DarknetConv2D_BN_Leaky(num_filters, (1, 1))(x)
+    x = DarknetConv2D_BN_Leaky(num_filters * 2, (3, 3))(x)
+    x = DarknetConv2D_BN_Leaky(num_filters, (1, 1))(x)
+    x = DarknetConv2D_BN_Leaky(num_filters * 2, (3, 3))(x)
+    x = DarknetConv2D_BN_Leaky(num_filters, (1, 1))(x)
+
+    # 将最后的通道数调整为outfilter
+    y = DarknetConv2D_BN_Leaky(num_filters * 2, (3, 3))(x)
+    y = DarknetConv2D(out_filters, (1, 1))(y)
+
+    return x, y
+
+# ---------------------------------------------------#
+#   特征层->最后的输出
+# ---------------------------------------------------#
+def yolo_body(inputs, num_anchors, num_classes):
+
+    # 生成darknet53的主干模型
+    feat1, feat2, feat3 = darknet_body(inputs)
+    darknet = Model(inputs, feat3)
+
+    # 第一个特征层
+    # y1=(batch_size,13,13,3,85)
+    x, y1 = make_last_layers(darknet.output, 512, num_anchors * (num_classes + 5))
+
+    x = compose(
+        DarknetConv2D_BN_Leaky(256, (1, 1)),
+        UpSampling2D(2))(x)
+    x = Concatenate()([x, feat2])
+    # 第二个特征层
+    # y2=(batch_size,26,26,3,85)
+    x, y2 = make_last_layers(x, 256, num_anchors * (num_classes + 5))
+
+    x = compose(
+        DarknetConv2D_BN_Leaky(128, (1, 1)),
+        UpSampling2D(2))(x)
+    x = Concatenate()([x, feat1])
+    # 第三个特征层
+    # y3=(batch_size,52,52,3,85)
+    x, y3 = make_last_layers(x, 128, num_anchors * (num_classes + 5))
+
+    return Model(inputs, [y1, y2, y3])
